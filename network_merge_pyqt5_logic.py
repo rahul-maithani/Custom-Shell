23569de4569af263e2827c3b4d18d0bea09116bf
Robin Gusain <gusainrobin37@gmail.com>
2025-05-15 01:51:52 +0530

    Replaced tkinter with PyQt5 for GUI and integrated frontend with backend

---
+++ network_merge_pyqt5_logic.py
@@ -0,0 +1,69 @@
+# network_pyqt.py
+from PyQt5 import QtWidgets
+from notNeeded.network_gui import Ui_MainWindow  # PyQt GUI
+from notNeeded.backend import shellBackend       # Logic
+import sys
+
+class NetworkQtWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.backend = shellBackend()
+        self.inputType = None
+
+        # Connect buttons
+        self.ui.pushButton.clicked.connect(self.ask_ping)
+        self.ui.pushButton_2.clicked.connect(self.tracert)
+        self.ui.pushButton_3.clicked.connect(self.hostname)
+        self.ui.pushButton_4.clicked.connect(self.ipconfig)
+        self.ui.pushButton_5.clicked.connect(self.ask_curl)
+        self.ui.pushButton_6.clicked.connect(self.ask_get_ip)
+        self.ui.pushButton_7.clicked.connect(self.close)
+
+    def ask_input(self, title, label, input_type):
+        self.inputType = input_type
+        text, ok = QtWidgets.QInputDialog.getText(self, title, label)
+        if ok and text:
+            self.process_input(text)
+
+    def ask_ping(self):
+        self.ask_input("Ping", "Enter the IP or URL to ping:", 'ping')
+
+    def ask_curl(self):
+        self.ask_input("Curl", "Enter the URL to curl:", 'curl')
+
+    def ask_get_ip(self):
+        self.ask_input("Get IP", "Enter the URL to get IP of:", 'getIp')
+
+    def process_input(self, inputRes):
+        if self.inputType == 'ping':
+            result = self.backend.pingRun(inputRes)
+        elif self.inputType == 'getIp':
+            result = self.backend.getIp(inputRes)
+        elif self.inputType == 'curl':
+            result = self.backend.curl(inputRes).stdout
+        else:
+            result = "Unknown input type."
+        QtWidgets.QMessageBox.information(self, "Result", result)
+
+    def tracert(self):
+        result = self.backend.tracertRun()
+        QtWidgets.QMessageBox.information(self, "Tracert", result)
+
+    def hostname(self):
+        result = self.backend.hostRun()
+        QtWidgets.QMessageBox.information(self, "Hostname", f"Hostname: {result}")
+
+    def ipconfig(self):
+        result = self.backend.ipconfig()
+        QtWidgets.QMessageBox.information(self, "IP Config", result)
+
+
+# To test standalone
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = NetworkQtWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ output.py
@@ -0,0 +1,130 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from notNeeded.filemang import fileManWindow
+from network_merge_pyqt5_logic import NetworkQtWindow
+from systeminfo_merged import SystemInfoApp
+from filemang_merge_pyqt5_logic import FileManQtWindow
+import notNeeded.bg_rc as bg_rc
+
+class Ui_MainWindow(object):
+    def show_loading(self):
+        self.loading_dialog = QtWidgets.QDialog()
+        self.loading_dialog.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Popup)
+        self.loading_dialog.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.loading_dialog.setAttribute(QtCore.Qt.WA_NoSystemBackground, True)
+        self.loading_dialog.setAttribute(QtCore.Qt.WA_OpaquePaintEvent, False)
+        self.loading_dialog.setStyleSheet("background-color: rgba(0,0,0,0);")
+        self.loading_dialog.setFixedSize(80, 80)
+
+        spinner = QtWidgets.QLabel(self.loading_dialog)
+        spinner.setStyleSheet("background-color: rgba(0,0,0,0);")
+        movie = QtGui.QMovie("spinner.gif")
+        movie.setScaledSize(QtCore.QSize(150, 150))
+        spinner.setMovie(movie)
+        spinner.setAlignment(QtCore.Qt.AlignCenter)
+        movie.start()
+
+        layout = QtWidgets.QVBoxLayout(self.loading_dialog)
+        layout.addWidget(spinner)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
+        center = self.centralwidget.geometry().center()
+        dlg_size = self.loading_dialog.size()
+        self.loading_dialog.move(self.centralwidget.mapToGlobal(
+            QtCore.QPoint(center.x() - dlg_size.width() // 2,
+                          center.y() - dlg_size.height() // 2)))
+        self.loading_dialog.show()
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(1100, 849)
+        MainWindow.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.CustomizeWindowHint |
+                                  QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint)
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 0, 1061, 791))
+        self.label.setMovie(QtGui.QMovie("animated2.gif"))
+        self.label.movie().start()
+        self.label.setScaledContents(True)
+
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(440, 130, 301, 51))
+        self.label_2.setStyleSheet("font: 75 24pt \"MS Shell Dlg 2\"; color: white;")
+
+        def make_button(y, color, hover_color, text, icon_path, icon_size):
+            btn = QtWidgets.QPushButton(self.centralwidget)
+            btn.setGeometry(QtCore.QRect(418, y, 280, 70))
+            btn.setStyleSheet(f"""
+                QPushButton {{
+                    background-color: {color};
+                    color: white;
+                    font: bold 20px;
+                    border-radius: 10px;
+                    padding: 20px;
+                }}
+                QPushButton:hover {{
+                    background-color: {hover_color};
+                }}
+            """)
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap(icon_path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            btn.setIcon(icon)
+            btn.setIconSize(QtCore.QSize(*icon_size))
+            btn.setText(f"    {text}")
+            return btn
+
+        self.pushButton = make_button(230, "#670D2F", "#A53860", "File Management", "FILE.jpeg", (40, 40))
+        self.pushButton_2 = make_button(330, "#344C64", "#57A6A1", "Network", "network.png", (60, 60))
+        self.pushButton_3 = make_button(430, "#1D267D", "#5C469C", "System Info", "SINFO.jpeg", (40, 40))
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        MainWindow.setMenuBar(QtWidgets.QMenuBar(MainWindow))
+        MainWindow.setStatusBar(QtWidgets.QStatusBar(MainWindow))
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(self.open_file_management)
+        self.pushButton_2.clicked.connect(self.open_network)
+        self.pushButton_3.clicked.connect(self.open_system_info)
+
+    def retranslateUi(self, MainWindow):
+        _t = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_t("MainWindow", "MainWindow"))
+        self.label_2.setText(_t("MainWindow", "Custom Shell"))
+
+    def open_file_management(self):
+        self.show_loading()
+        QtCore.QTimer.singleShot(1000, self._launch_file_management)
+
+    def _launch_file_management(self):
+        self.file_window = FileManQtWindow()
+        self.file_window.show()
+        self.loading_dialog.close()
+
+    def open_network(self):
+        self.show_loading()
+        QtCore.QTimer.singleShot(1000, self._launch_network)
+
+    def _launch_network(self):
+        self.file_window = NetworkQtWindow()
+        self.file_window.show()
+        self.loading_dialog.close()
+
+    def open_system_info(self):
+        self.show_loading()
+        QtCore.QTimer.singleShot(1000, self._launch_system_info)
+
+    def _launch_system_info(self):
+        self.file_window = SystemInfoApp()
+        self.file_window.show()
+        self.loading_dialog.close()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
